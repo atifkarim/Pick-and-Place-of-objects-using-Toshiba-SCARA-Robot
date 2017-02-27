4821232ebf42f456b99dd908dc21e503c96b94eb
SCARA <toshiba.robot650@gmail.com>
2017-02-27 17:38:31 +0100

    cleared few files

--- src/receive_command.cpp
+++ src/receive_command.cpp
@@ -1,3 +1,11 @@
+
+/**********************************************************************************************************************************
+
+  Description:This program is used for communication with the robot(TH650) Controller.
+  Auther     :Hirenbhai Patel & Avinash Jain
+
+ **********************************************************************************************************************************/
+
 #include <ros/ros.h>
 #include"std_msgs/String.h"
 #include"robot_communication/connection_msg.h"
@@ -17,7 +25,9 @@
 using namespace std;
 int m_socket;
 
+//Function for making the socket communication with particular protocol
 bool ClientConnect(const char* host, int port, int iIdSocket)
+
 {
   sockaddr_in addr;
   addr.sin_family = AF_INET;
@@ -38,50 +48,56 @@ void chatterCallback(const std_msgs::String::ConstPtr& msg)
   bool b_Send;
   double x,y,z,c;
   ros::NodeHandle n;
-  ros::Publisher pub = n.advertise<robot_communication::current_pos_robot>("/robot_current_pos",1000);
+  ros::Publisher pub = n.advertise<robot_communication::current_pos_robot>("/robot_current_pos",1000); // creat publisher which publish a data on a topic "/robot_current_pos"
   robot_communication::current_pos_robot robot_pose_msg;
   QString toSplitString;
 
   strcpy(c_Send,msg->data.c_str());
-  b_Send = write(m_socket,c_Send,strlen(c_Send));
+  b_Send = write(m_socket,c_Send,strlen(c_Send)); // send the data to the robot controller
   sleep(1);
 
-  recv(m_socket,ReadString,500,0);
+  recv(m_socket,ReadString,500,0); // read the data from robot controller
   toSplitString = QString::fromUtf8(ReadString);
-  QStringList ReceiveList = toSplitString.split(" "); //Konvertierung in QStringlist
-  ReceiveList.removeFirst(); //entferne "LESE"
+  QStringList ReceiveList = toSplitString.split(" "); //convert data in QStringlist
+  ReceiveList.removeFirst(); //remove "LESE" from the data
   for(int i = 0; i < ReceiveList.size(); i++)
   {
     if(ReceiveList.value(i).toDouble() == 0)
     {
-      ReceiveList.removeAt(i); //entferne Leerstellen
-      i = i - 1;				//verschiebe die Schleife bei L�schung
+      ReceiveList.removeAt(i); //Remove any empty spaces
+      i = i - 1;
     }
   }
-  x = ReceiveList.value(0).toDouble(); //Werte werden Zugeordnet
+
+  //assign the values
+  x = ReceiveList.value(0).toDouble();
   y = ReceiveList.value(1).toDouble();
   z = ReceiveList.value(2).toDouble();
   c = ReceiveList.value(3).toDouble();
 
+  cout<<"x ="<<x<<endl;
+  cout<<"y ="<<y<<endl;
+  cout<<"z ="<<z<<endl;
+  cout<<"c ="<<c<<endl;
 
   robot_pose_msg.PosX=x;
   robot_pose_msg.PosY=y;
   robot_pose_msg.PosZ=z;
   robot_pose_msg.PosC=c;
-  pub.publish(robot_pose_msg);
+  pub.publish(robot_pose_msg); //publish the current position of the robot on a topic "/robot_current_pos"
 }
 
 int main(int argc, char **argv)
 {
   // Set up ROS.
-  ros::init(argc, argv, "receive_command");
-  ros::NodeHandle n;
+  ros::init(argc, argv, "receive_command"); //create a node name "receive_command"
+  ros::NodeHandle n; //create a object for node handle
   int iPort = PORT;
   bool bConnected;
-  const char* c_Host = "192.168.0.124";
+  const char* c_Host = "192.168.0.124"; //ip address of the robot controller
 
-  m_socket = socket(AF_INET, SOCK_STREAM, 0);
-  bConnected = ClientConnect(c_Host,iPort,m_socket);
+  m_socket = socket(AF_INET, SOCK_STREAM, 0); //create a socket
+  bConnected = ClientConnect(c_Host,iPort,m_socket); //do connection with the socket
 
   if(bConnected == true)
   {
@@ -90,10 +106,10 @@ int main(int argc, char **argv)
   else
     cout << "Notconnected";
 
-  ros::Subscriber sub = n.subscribe("/send_string", 1000, chatterCallback);
+  ros::Subscriber sub = n.subscribe("/send_string", 1000, chatterCallback); // subscribe a topic "/send_string"
 
   ros::spin();
-  close(m_socket);
+  close(m_socket); //close the socket
 
   return 0;
 }
--- src/send_command.cpp
+++ src/send_command.cpp
@@ -1,47 +1,29 @@
+
+/**********************************************************************************************************************************
+
+  Description:This program is used for sending the string data to receive_command node.
+  Auther     :Hirenbhai Patel & Avinash Jain
+
+ **********************************************************************************************************************************/
+
 #include <ros/ros.h>
 #include"std_msgs/String.h"
 #include<vector>
 #include<string.h>
+#include"pick_and_place/object_coordinate.h"
 
+void object_to_string(const pick_and_place::object_coordinate msg1)
+{
 
-//std::string DoubleToRobotString(double Funct, double X, double Y, double Z, double C, double T, double Shoudler, double Speed, double G)
-//{
-//  std::vector<double> Input;
-//  Input.push_back(Funct);
-//  Input.push_back(X);
-//  Input.push_back(Y);
-//  Input.push_back(Z);
-//  Input.push_back(C);
-//  Input.push_back(T);
-//  Input.push_back(Shoudler);
-//  Input.push_back(Speed);
-//  Input.push_back(G);
-
-//  std::string RobotString;
-//  for(int i = 0; i < 9; i++){
-//    if(i == 8){
-//      RobotString = RobotString+QString::number(Input[i], 'g', 6)+"\r\n";
-//      break;
-//    }
-//    RobotString = RobotString+QString::number(Input[i], 'g', 6)+",";
-//  }
-//  return RobotString;
-//}
-
-//char *StringToCharP(std::string InputString){
-//  std::string str = InputString.toStdString();
-//  char *cstr = new char[str.length() + 1];
-//  strcpy(cstr, str.c_str());
-//  return cstr;
-//}
-
+}
 
 int main(int argc, char **argv)
 {
   // Set up ROS.
-  ros::init(argc, argv, "send_command");
+  ros::init(argc, argv, "send_command"); //create a node name "send_command"
   ros::NodeHandle n;
-  ros::Publisher send_command_pub = n.advertise<std_msgs::String>("/send_string", 1000);
+  ros::Publisher send_command_pub = n.advertise<std_msgs::String>("/send_string", 1000);// create publisher which publish a data on a topic "/send_string"
+  ros::Subscriber receive_object = n.subscribe<pick_and_place::object_coordinate>("/object_current_pos",10, object_to_string);
   ros::Rate loop_rate(10);
 
   char c_Send[30];
@@ -52,14 +34,11 @@ int main(int argc, char **argv)
 
   while (ros::ok())
   {
-    send_command_pub.publish(msg);
+    send_command_pub.publish(msg); //publish the data on a topic "/send_string"
     ros::spinOnce();
     //ROS_INFO("%s",msg.data);
     loop_rate.sleep();
 
   }
-
   return 0;
-
-
 }
